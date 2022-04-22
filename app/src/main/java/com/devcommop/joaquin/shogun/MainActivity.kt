f9be671c4a1c66985400dcf413940398015f8a03
Joaquin144 <vvpp898.comco@gmail.com>
2022-04-22 14:37:31 +0530

    check

--- app/src/main/java/com/devcommop/joaquin/shogun/MainActivity.kt
+++ app/src/main/java/com/devcommop/joaquin/shogun/MainActivity.kt
@@ -13,45 +13,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        /* Our job is to display a webpage using webview and run some android code when its elements are accessed
-        Steps :--
-        1.) INTERNET permission in Manifest
-        2.) webview in xml
-        3.) load your url keeping javascript as enabled
-        4.) define a client and assign it to the webview
-        5.)
-         */
-        val webView: WebView = findViewById(R.id.webView)
-        webView.loadUrl("https://cheezycode.com")
-        webView.settings.javaScriptEnabled = true //Android's WARNING :-- This can introduce XSS vulnerabilities in app
-        webView.webViewClient = object : WebViewClient(){
-            override fun onPageFinished(view: WebView?, url: String?) {
-                super.onPageFinished(view, url)
-                injectJS(view)
-            }
-        }
-        webView.addJavascriptInterface(JsAndroidBridge,"Bridge")
+        //Aim : Map- SDK demonstration
 
-    }
-
-    private fun injectJS(view: WebView?) {
-        view!!.loadUrl(
-            """
-                javascript: (function(){
-                    let header = document.querySelector(".header-widget");
-                    header.addEventListener("click",function(){
-                        Bridge.calledFromJS();
-                    })
-                })()
-            """
-        )
-    }
-
-    object JsAndroidBridge{
-        @JavascriptInterface
-        fun calledFromJS(){
-            Log.d("####","You called android function using JS")
-            //Toast.makeText(this@MainActivity,"You called android function using JS",Toast.LENGTH_SHORT).show()
-        }
     }
 }
