fb19a971951282487c69e781bc8bdd127d1e5cd1
Joaquin144 <vvpp898.comco@gmail.com>
2022-05-03 20:24:29 +0530

    initiated

--- app/src/main/java/com/devcommop/joaquin/shogun/MainActivity.kt
+++ app/src/main/java/com/devcommop/joaquin/shogun/MainActivity.kt
@@ -7,13 +7,17 @@ import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import android.widget.Toast
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        //Aim : Map- SDK demonstration
 
     }
 }
